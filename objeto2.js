6e7d4487eb9d334dc5c1d5d180bd4ce1dcd5ae7a
juliavazz99 <juliamoreira@colegiopaulodetarso.com.br>
2025-06-23 16:14:59 +0000

    t

---
+++ objeto2.js
@@ -0,0 +1,9 @@
+const livro = {
+  titulo: "Dom Casmurro",           // string
+  paginas: 256,                     // number
+  autores: ["Machado de Assis"],   // array
+  editora: {                       // object (filho)
+    nome: "Editora ABC",
+    anoFundacao: 1995
+  }
+};
